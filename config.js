d4fa4ab7c113c5e16eca5b540da2dac8f557790c
Amith S Dev <70939150+AmithSahadevan@users.noreply.github.com>
2025-11-22 00:07:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuration file for the application
+const config = {
+    streamlitUrl: 'https://590e-2401-4900-6149-aef1-8841-6059-718b-b42f.ngrok-free.app'  // This will be updated whenever ngrok URL changes
+}; 
